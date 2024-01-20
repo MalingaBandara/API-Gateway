250affdf765f63285fbecdc447b4b8123fcc1b26
Malinga Bandara <malinga1021@gmail.com>
2024-01-20 13:15:03 +0530

    Add Logger

--- api-gateway/src/main/java/com/bitlord/microservices/apigateway/LoggingFilter.java
+++ api-gateway/src/main/java/com/bitlord/microservices/apigateway/LoggingFilter.java
@@ -1,18 +1,25 @@
 package com.bitlord.microservices.apigateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.web.server.ServerWebExchange;
+
 
 import reactor.core.publisher.Mono;
 
 
 public class LoggingFilter implements GlobalFilter {
 	
+	
+	// Add Logger
+	private Logger logger = LoggerFactory.getLogger( LoggingFilter.class );
+	
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
