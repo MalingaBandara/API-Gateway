4cc9729685b66985448a8a925cc64cac27c52932
Malinga Bandara <malinga1021@gmail.com>
2024-01-20 13:06:45 +0530

    Add Unimplement methods

--- api-gateway/src/main/java/com/bitlord/microservices/apigateway/LoggingFilter.java
+++ api-gateway/src/main/java/com/bitlord/microservices/apigateway/LoggingFilter.java
@@ -1,9 +1,21 @@
 package com.bitlord.microservices.apigateway;
 
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
 
 
 public class LoggingFilter implements GlobalFilter {
+	
 
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 	
 }
